df5c23e7de1889e609ba06443d2cf9db58c86f00
Punnyartha Banerjee <punnyarthabanerjee@gmail.com>
2023-07-31 21:54:47 +0530

    setup with vite

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+    </div>
+  )
+}
