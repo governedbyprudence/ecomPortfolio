629f7fb33ab65f59e8bf3e34361383a8b3d8e95d
Punnyartha Banerjee <punnyarthabanerjee@gmail.com>
2023-08-20 13:28:46 +0530

    Single product card basic added

--- src/core/components/product.jsx
+++ src/core/components/product.jsx
@@ -1,8 +1,11 @@
 export default function Product(props) {
     const product = props.product
     return (
-        <div>
-            product
+        <div className="flex flex-col h-[400px] lg-h[500px] justify-center items-center m-10">
+            <div className="h-full bg-black m-5 w-[70%]">Image</div>
+            <h2 className="text-xl font-bold mt-2">{product.name}</h2>
+            <h4 className="mt-2">{product.description}</h4>
+            <h1 className="my-2 text-xl font-bold">₹ {product.price}</h1>
         </div>
     );
 }
--- src/core/routes/products.jsx
+++ src/core/routes/products.jsx
@@ -17,7 +17,7 @@ export const AllProductPage = ()=>{
         <CustomNavbar/>
           <h1 className="text-2xl font-bold m-8">Our Products</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 m-8">
-          {data.map((e)=><Product/>)}
+          {data.map((e)=><Product product={e}/>)}
 
         </div>
      </div>
