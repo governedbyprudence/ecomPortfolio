0dd36da2f7966ca7657b5323265df5f8543c931c
Punnyartha Banerjee <punnyarthabanerjee@gmail.com>
2023-08-20 16:51:48 +0530

    contact page made responsive

--- src/core/routes/contactpage.jsx
+++ src/core/routes/contactpage.jsx
@@ -4,16 +4,16 @@ const ContactPage = () => {
     return(
         <div>
             <CustomNavbar/>
-            <div className="h-screen w-full flex flex-row justify-center">
+            <div className="h-screen w-full flex flex-col lg:flex-row justify-center items-center mt-[200px] lg:mt-[-50px]">
                 <div className="w-full p-4 m-10">
-                    <h1 className=" text-6xl p-10">Contact Us</h1>
+                    <h1 className=" text-3xl md:text-4xl lg:text-6xl px-5 font-bold lg:font-normal">Contact Us</h1>
                     <p className="p-5">We're here to help and answer any questions you might have.<br/> Please feel free to reach out to us</p>
                     <p className="p-5">If you require assistance with an order, have questions about our products or services, or need help troubleshooting an issue, our dedicated customer support team is ready to assist you.</p>
                     <p className="p-5">Email: <span className="font-bold">support@yourwebsite.com</span></p>
                     <p className="p-5">Phone: <span className="font-bold">(123) 456-7890</span></p>
                 </div>
-                <div className=" w-full px-10">
-                    <div className=" rounded-xl mr-[100px] mt-[10px] pl-[10px] pr-[30px] py-2 bg-gray-100 h-[600px] shadow-2xl">
+                <div className=" w-full px-20">
+                    <div className=" rounded-xl mr-[50px] lg:mr-100 mt-[10px] pl-[10px] pr-[30px] py-2 bg-gray-100 h-[600px] shadow-2xl">
                     <h1 className=" text-xl font-bold px-5 pt-5" >Send us your query !</h1>
                         <form className="flex flex-col">
                             <input type="text" placeholder="Full Name" className=" bg-blue-grey-100 mx-4 my-3 p-2 rounded-2xl" id="name"></input>
